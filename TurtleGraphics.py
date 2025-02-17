2d4186a0093aec3e9d41e512d4e4154ae23fa9e7
owenbett <olbetts@cox.net>
2025-02-16 23:52:49 -0600

    Not Final Commit

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,7 +1,7 @@
 #TurtleGraphics.py
-#Name:
-#Date:
-#Assignment:
+#Name: Owen Betts
+#Date: 02.16.2025
+#Assignment: Practical Scripting Lab 4 
 
 import turtle #needed generally but not in CodeHS
 hideturtle() #hides the default turtle in CodeHS
@@ -11,16 +11,65 @@ def drawSquare(myTurtle, size):
         myTurtle.forward(size)
         myTurtle.right(90)
 
+def drawPolygon(bob, sides):
+    for s in range(sides):
+        bob.forward(50)
+        bob.right(360/sides)
+              
+#def fillCorner(alice, corner):
+    drawSquare(alice, 100)
+    
+    if corner == 1:
+        alice.begin_fill()
+        drawSquare(alice, 50)
+        alice.end_fill()
+    elif corner == 2:
+        alice.right(90)
+        alice.forward(50)
+        alice.left(90)
+        alice.begin_fill()
+        drawSquare(alice, 50)
+        alice.end_fill()
+        
 
 def main():
     myTurtle = turtle.Turtle()
-    # drawPolygon(myTurtle, 5) #draws a pentagon
-    # drawPolygon(myTurtle, 8) #draws an octogon
 
+    
+    
+    
+    #drawSquare(myTurtle, 100)
+    
+    # drawPolygon(myTurtle, 5) #draws a pentagon
+    
+    # drawPolygon(myTurtle, 8) #draws an octogon
+    
     # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
+
     # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
 
     # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
+
+    
+    def drawSquare(size):
+        for i in range (4):
+            turtle.forward(100)
+            turtle.right(90)
+            
+    def drawSquaresInSquares(numSquares, spacing):
+        for i in range(numSquares):
+            drawSquare(start_side - i * spacing)
+            turtle.penup()
+            turtle.backward(spacing / 2)
+            turtle.right(90)
+            turtle.forward(spacing / 2)
+            turtle.left(90)
+            turtle.pendown()
+            
+    numSquares=5 
+    spacing=40
+    
+
     # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
 
 
